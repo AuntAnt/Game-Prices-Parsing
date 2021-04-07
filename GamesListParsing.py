d7d62c074fc142e6cdf9bbd185c7706cab470a54
Anton Kuzmin1 <Anton_Kuzmin1@epam.com>
2021-04-07 22:29:14 +0400

    GamesListParsing.py

--- GamesListParsing.py
+++ GamesListParsing.py
@@ -1,13 +1,27 @@
 from bs4 import BeautifulSoup
 import requests as req
 import string
+import httplib2
+import apiclient
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import date
+
+CREDENTIALS_FILE = 'game-wish-list-174a4b364e4b.json'
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    CREDENTIALS_FILE,
+    ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+)
+
+httpAuth = credentials.authorize(httplib2.Http())
+service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
 
 
-# 1.[DONE] добавить трим ценника
-# 2. добавить чтение url из файлов
-# 3. [IN PROGRESS]добавить запись в гугл-таблицу название игры + цена + дата обновления
-# 4. [DONE]добавить запись в гугл-таблицу
-# 5*. реализовать UI (???)
+# 1.[DONE]добавить трим ценника
+# 2.[DONE]добавить чтение url гугл-таблицы
+# 3.[DONE]добавить запись в гугл-таблицу название игры + цена + дата обновления
+# 4.[DONE]добавить запись в гугл-таблицу
+# 5*.реализовать UI (???)
 def extract_block(url):
     link = req.get(url).text
     soup = BeautifulSoup(link, 'lxml')
@@ -31,11 +45,97 @@ def extract_name(block):
     return name
 
 
-valhalla = "https://store.playstation.com/ru-ru/product/EP0001-PPSA01532_00-GAME000000000000"
-chronos = "https://store.playstation.com/ru-ru/product/EP4389-CUSA15161_00-CHRONOSEU0000000"
+def permission_to_spreadsheet(spreadsheet_id):
+    drive_service = apiclient.discovery.build('drive', 'v3', http=httpAuth)
+    access = drive_service.permissions().create(
+        fileId=spreadsheet_id,
+        body={'type': 'user', 'role': 'writer', 'emailAddress': 'furthertwoant@gmail.com'},
+        fields='id'
+    ).execute()
+
+    return access
 
 
-ext_price = extract_price(extract_block(chronos))
-ext_name = extract_name(extract_block(chronos))
+def write_into_sheet(game, price, update_date, spreadsheet_id, cell_range):
+    results = service.spreadsheets().values().batchUpdate(
+        spreadsheetId=spreadsheet_id,
+        body={
+            "valueInputOption": "USER_ENTERED",
+            "data": [
+                {
+                    "range": cell_range,
+                    "majorDimension": "ROWS",
+                    "values": [
+                        [game, price, update_date]
+                    ]
+                }
+            ]
+        }).execute()
 
-print(ext_name, ext_price)
+    return results
+
+
+def clear_sheet(spreadsheet_id, cell_range):
+    results = service.spreadsheets().values().clear(
+        spreadsheetId=spreadsheet_id,
+        range=cell_range
+    ).execute()
+
+    return results
+
+
+def read_from_sheet(spreadsheet_id):
+    cell_range = ["Games!D2:D100"]
+    results = service.spreadsheets().values().batchGet(
+        spreadsheetId=spreadsheet_id,
+        ranges=cell_range,
+        valueRenderOption='FORMATTED_VALUE',
+        dateTimeRenderOption='FORMATTED_STRING'
+    ).execute()
+
+    sheet_values = results['valueRanges'][0]['values']
+    return sheet_values
+
+
+spreadsheet_id = "18hrtQcTbQMFTfYkntgMYjkP_lFB6k5MQjjOxyI9y_R0"
+update_date = date.today().strftime("%d-%m-%Y")
+
+# permission_to_spreadsheet(spreadsheet_id)
+
+range_a = 2
+range_c = 2
+
+
+# Изменить cell_range - нужно удалять кол-во записей, которые уже есть в таблице, а не которые добавляются
+# Сделать вместе с добавлением четния url из таблицы
+def cleaning_sheet(range_a, range_c, urls):
+    for item in range(len(urls) - (len(urls) - 1)):
+        cell_range = "Games!A" + str(range_a) + ":C" + str(range_c)
+        range_a += 1
+        range_c += 1
+        clear_sheet(spreadsheet_id, cell_range)
+
+
+def adding_games(range_a, range_c, games):
+    for game in games:
+        ext_price = extract_price(extract_block(game))
+        ext_name = extract_name(extract_block(game))
+        for item in range(len(games) - (len(games) - 1)):
+            cell_range = "Games!A" + str(range_a) + ":C" + str(range_c)
+            range_a += 1
+            range_c += 1
+            write_into_sheet(ext_name, ext_price, update_date, spreadsheet_id, cell_range)
+
+
+urls = read_from_sheet(spreadsheet_id)
+
+games = []
+
+for url in urls:
+    for i in url:
+        game = i
+        games.append(game)
+
+
+cleaning_sheet(range_a, range_c, games)
+adding_games(range_a, range_c, games)
